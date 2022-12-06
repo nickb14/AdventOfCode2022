37dba7639ae23dc300b8ac781dec1512cbeeed28
nickb14 <nbilsborrow@gmail.com>
2022-12-06 13:19:48 -0500

    d05p2 done

---
+++ day05/d05p2.py
@@ -0,0 +1,40 @@
+def main():
+
+    lines = []
+
+    with open("day05/input.txt") as f:
+            line = f.readline()
+            while line:
+                lines.append(line)
+                line = f.readline()
+    
+    index = lines.index('\n')
+    numOfStacks = (len(lines[index-1])) // 4
+
+    stacks = []
+    bottomRow = lines[index-2]
+    for j in range(numOfStacks):
+        stacks.append([bottomRow[j*4+1]])
+    for i in range(index-2): #I assumed that there are only single digit stack numbers
+        row = lines[index-3-i] #this algorithm was going to be so good in my brain... but alas so many restrictions
+        for j in range(numOfStacks):
+            crate = row[j*4+1]
+            if crate != ' ':
+                stacks[j].append(crate)
+
+    procedure = lines[index+1:]
+    for line in procedure:
+        space = line.find(" from")
+        m = int(line[5:space])
+        f = int(line[space+6]) - 1 #indexing be like
+        t = int(line[space+11]) - 1 #also singel digit things...
+        height = len(stacks[t])
+        for x in range(m):
+            stacks[t].insert(height, stacks[f].pop()) #changing 2 lines of code be like
+    
+    final = ""
+    for stack in stacks:
+        final += stack[-1]
+    print(final)
+
+main()
